0efd2d1fb1f0855e234a344ad068bec0e915f8e4
Wesley Clements <wesley.clements@gmail.com>
2023-07-03 16:50:49 -0400

    fixed permission issues

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-import { Client, GatewayIntentBits } from "discord.js";
-const client = new Client({ intents: [GatewayIntentBits.MessageContent] });
+import "dotenv/config";
+import { Client, Events, GatewayIntentBits } from "discord.js";
 
-client.on("messageCreate", (message) => {
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once(Events.ClientReady, client => {
+  console.log(`Ready! Logged in as ${client.user.tag}`);
+});
+
+client.on(Events.MessageCreate, async (message) => {
   if (message.author.bot) return;
-  if (message.content.match(/bruh/ig)) {
-    message.channel.send("bruh");
+  if (!message.content.match(/bruh/ig)) return;
+  const bruhEmoji = message.guild?.emojis.cache.find(emoji => emoji.name === "bruh");
+  if (!bruhEmoji) {
+    await message.channel.send("bruh");
+    return;
   }
+  await message.react(bruhEmoji);
+  await message.channel.send(`bruh ${bruhEmoji}`);
 });
 
 client.login(process.env.DISCORD_TOKEN);
